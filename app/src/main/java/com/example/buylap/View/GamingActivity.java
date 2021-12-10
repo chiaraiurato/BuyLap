a0a7ec1c3b577a5acff1e06c013ba589ccd043eb
chiaraiurato <chiara.iurato@gmail.com>
2021-12-10 16:13:16 +0100

    Try webView with amazon

--- app/src/main/java/com/example/buylap/View/GamingActivity.java
+++ app/src/main/java/com/example/buylap/View/GamingActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.buylap.MostViewAdapter;
 import com.example.buylap.MostViewed;
@@ -13,25 +15,15 @@ import com.example.buylap.R;
 import java.util.ArrayList;
 
 public class GamingActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RecyclerView.Adapter adapter;
-        RecyclerView recyclerList;
+
         setContentView(R.layout.activity_gaming);
-        recyclerList=(RecyclerView)findViewById(R.id.RecyclerList);
-
-        LinearLayoutManager linearLayoutManager3 = new LinearLayoutManager(this,  LinearLayoutManager.VERTICAL, false);
-        recyclerList.setLayoutManager(linearLayoutManager3);
-
-        ArrayList<MostViewed> mostviewed = new ArrayList<>();
-        mostviewed.add(new MostViewed("AMD", "Ryzen 7 3700X", "ic_baseline_cpu"));
-        mostviewed.add(new MostViewed("NVIDIA", "GTX 1800ti", "ic_baseline_gpu"));
-        mostviewed.add(new MostViewed("CORSAIR", "DDR4 32GB", "ic_baseline_ram"));
-        mostviewed.add(new MostViewed("Cooler Master", "MasterBox Q300L", "ic_baseline_laptop_24"));
-        adapter= new MostViewAdapter(mostviewed);
-        recyclerList.setAdapter(adapter);
+        webView=findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.amazon.it/");
 
     }
 }
