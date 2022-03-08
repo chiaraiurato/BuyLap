aec73a52b75dbff01436166aa76fd79dfaaa39c2
chiaraiurato <chiara.iurato@gmail.com>
2022-03-08 17:24:50 +0100

    -installing Selenium libraries

---
+++ app/src/main/java/test/TestSelenium.java
@@ -0,0 +1,28 @@
+package test;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+    public static void main(String[] args) throws Exception {
+
+        openDriver();
+    }
+
+    public static String openDriver() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "app/src/androidTest/java/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://scholar.google.com/citations?user=PQWZTB8AAAAJ&hl=it");
+        WebElement txtBox=driver.findElement(By.xpath("//*[@id=\"gsc_rsb_st\"]/tbody/tr[1]/td[2]"));
+        Thread.sleep(15000);
+        driver.quit();
+        return txtBox.getText();
+
+
+    }
+
+
+}
